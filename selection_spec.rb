b3449d5b6c4f680b5feeb811e966e5fcb19dd614
Andrew <andrewmbyrd@gmail.com>
2017-03-16 12:27:50 -0500

    moved selection_spec.rb

---
+++ selection_spec.rb
@@ -0,0 +1,19 @@
+include RSpec
+require 'bloc_record/base'
+require 'bloc_record'
+require_relative 'address-bloc-1/models/entry'
+
+RSpec.describe Entry, type: Class do
+
+
+
+  BlocRecord.connect_to('address-bloc-1/db/address_bloc.sqlite')
+
+  describe "#find_each" do
+    it "gets all instances of Entry" do
+      expect(Entry.find_each).to eq Enumerable
+    end
+  end
+
+
+end
