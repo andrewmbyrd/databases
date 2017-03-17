50b5218bd0bbf67465201dd76feab48cea720d28
Andrew <andrewmbyrd@gmail.com>
2017-03-17 14:21:36 -0500

    Completed assignment work (except where.not)

--- bloc_record/lib/bloc_record/persistence.rb
+++ bloc_record/lib/bloc_record/persistence.rb
@@ -52,26 +52,41 @@ module Persistence
        new(data)
      end
 
-     #id is an integer. updates is a hash
+     #id is an integer. updates is a hash typically.
+     #now for the assignment, we're updating it such that ids is an array of id's (integers) and updates is
+     #an array of hashes that contain data like {attribute => value}
      def update(ids, updates)
-       updates = BlocRecord::Utility.convert_keys(updates)
-       updates.delete "id"
-
-       updates_array = updates.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
-
-       if ids.class == Fixnum
-         where_clause = "WHERE id = #{ids};"
-       elsif ids.class == array
-         where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+       if ids.class == Array && updates.class == Array
+         #we can assume ids and updates will be of the same length
+         for i in 0...ids.length
+           #for each hash in the updates array, make sure the key is of type string for the SQL query
+           update = BlocRecord::Utility.convert_keys(updates[i])
+           connection.execute <<-SQL
+              UPDATE #{table}
+              SET #{update.keys.first} = #{BlocRecord::Utility.sql_strings(update.values.first)}
+              WHERE id = #{ids[i]}
+           SQL
+         end
        else
-         where_clause = ";"
-       end
+         updates = BlocRecord::Utility.convert_keys(updates)
+         updates.delete "id"
 
-       connection.execute <<-SQL
-          UPDATE #{table}
-          SET #{updates_array * ","}
-          where_clause
-        SQL
+         updates_array = updates.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+
+         if ids.class == Fixnum
+           where_clause = "WHERE id = #{ids};"
+         elsif ids.class == Array
+           where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+         else
+           where_clause = ";"
+         end
+
+         connection.execute <<-SQL
+            UPDATE #{table}
+            SET #{updates_array * ","}
+            where_clause
+          SQL
+        end
 
         true
      end
